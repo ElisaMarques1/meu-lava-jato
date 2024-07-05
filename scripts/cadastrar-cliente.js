3569604ecb1678fdd776d4cce6d2f3a1d480a952
elisa marques <elisamarques466@gmail.com>
2024-07-05 20:58:37 -0300

    achando erros do lava jato

---
+++ scripts/cadastrar-cliente.js
@@ -0,0 +1,25 @@
+const alertaAdicionarNovoCliente = require("../scripts/alerta");
+var bancoDeDados = require("../scripts/banco-de-dados");
+
+
+function adicionarProduto(event) {
+  event.preventDefault();
+
+  var cliente = document.getElementById('cliente').value
+  var carro = document.getElementById('carro').value
+  var placa = document.getElementById('placa').value
+  var queryInseririr = `INSERT INTO clientes (cliente, carro, placa) VALUES ('${cliente}', '${carro}', '${placa}')`;
+
+  bancoDeDados.query(queryInseririr, function (error) { 
+    if (error) {  
+      console.log(`Ocorreu um erro ao adicionar o cliente: ${error.code}`);
+      console.log(`Ocorreu um erro ao adicionar o cliente: ${error.fatal}`);
+    } else {    
+      console.log('CLIENTE ADICIONADO COM SUCESSO AO BANCO DE DADOS');
+      alertaAdicionarNovoCliente()
+    }
+  });
+}
+
+var formulario = document.getElementById('formulario')
+formulario.addEventListener('submit', adicionarProduto)
